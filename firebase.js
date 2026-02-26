c0964a8418c9a9f5c32d6d9383c058db91e531ff
JayFrostX <terrylinville04@gmail.com>
2026-02-26 17:12:59 -0500

    Initialize Firebase with configuration settings

--- firebase.js
+++ firebase.js
@@ -1,1 +1,10 @@
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
