03167c95af36001ffdf568d418f7c792283121eb
JayFrostX <terrylinville04@gmail.com>
2026-02-26 18:16:41 -0500

    Update Firebase configuration with new credentials
    Updated Firebase configuration with new credentials.

--- firebase.js
+++ firebase.js
@@ -1,8 +1,21 @@
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+apiKey: "AIzaSyDbjlgdS3mspldvDMJus9LrNI_6gItemBE",
+
+  authDomain: "swat-e2aa8.firebaseapp.com",
+
+  projectId: "swat-e2aa8",
+
+  storageBucket: "swat-e2aa8.firebasestorage.app",
+
+  messagingSenderId: "504240588895",
+
+  appId: "1:504240588895:web:4fa1aab70b57e65cae73ea",
+
+  measurementId: "G-1Y3Q37S9L1"
+
 };
+
+
 
 firebase.initializeApp(firebaseConfig);
 
